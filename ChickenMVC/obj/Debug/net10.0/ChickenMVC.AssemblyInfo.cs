9460a3065e10a797eb6bad8620254d3717c9ea47
Gerard Alexander Gibson <gagibson882@gmail.com>
2026-05-21 10:49:12 +0200

    ggg

--- ChickenMVC/obj/Debug/net10.0/ChickenMVC.AssemblyInfo.cs
+++ ChickenMVC/obj/Debug/net10.0/ChickenMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChickenMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b14a246b5d272808bae296f80eee6132fb8ff7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f0fabf14e0ed0682644a8a3cc70432a6538f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChickenMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChickenMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
